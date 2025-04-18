7ea79d8b3bbc5600147cde3da621d37459cd2066
Bethel Eyo <betheleyo@gmail.com>
2025-04-18 20:10:21 +0100

    Pricing Implementation
    Fourth Section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,6 @@
+import CheckIcon from "@/assets/check.svg";
+import clsx from "clsx";
+
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +54,68 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and exclusive features.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map((tier, index) => (
+            <div
+              key={index}
+              className={clsx(
+                "p-10 border border-[#222222]/10 rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full",
+                tier.inverse === true && "border-black bg-black text-white"
+              )}
+            >
+              <div className="flex justify-between">
+                <h3
+                  className={clsx(
+                    "text-lg font-bold text-black/50",
+                    tier.inverse === true && "text-white/50"
+                  )}
+                >
+                  {tier.title}
+                </h3>
+                {tier.popular && (
+                  <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                    <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2ED,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                      Popular
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="flex items-baseline gap-1 mt-[30px]">
+                <span className="text-4xl font-bold tracking-tighter leading-none">
+                  ${tier.monthlyPrice}
+                </span>
+                <span className="tracking-tight font-bold text-black/50">/month</span>
+              </div>
+              <button
+                className={clsx(
+                  "btn btn-primary w-full mt-[30px]",
+                  tier.inverse === true && "bg-white text-black"
+                )}
+              >
+                {tier.buttonText}
+              </button>
+              <ul className="flex flex-col gap-5 mt-8">
+                {tier.features.map((feature, index) => (
+                  <li key={index} className="text-sm flex items-center gap-4">
+                    <CheckIcon className="h-6 w-6" />
+                    <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
