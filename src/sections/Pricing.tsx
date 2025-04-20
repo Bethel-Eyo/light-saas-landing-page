08757706d8ebc36f04912f7d122040bd415efbb3
Bethel Eyo <betheleyo@gmail.com>
2025-04-20 17:14:22 +0100

    Pricing Animation Implementation

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,7 @@
+"use client";
 import CheckIcon from "@/assets/check.svg";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const pricingTiers = [
   {
@@ -81,9 +83,20 @@ export const Pricing = () => {
                 </h3>
                 {tier.popular && (
                   <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                    <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2ED,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                    <motion.span
+                      animate={{
+                        backgroundPositionX: "100%",
+                      }}
+                      transition={{
+                        duration: 1,
+                        repeat: Infinity,
+                        ease: "linear",
+                        repeatType: "loop",
+                      }}
+                      className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2ED,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2ED,#3BFFFF)] [background-size:200%] text-transparent bg-clip-text font-medium"
+                    >
                       Most Popular
-                    </span>
+                    </motion.span>
                   </div>
                 )}
               </div>
